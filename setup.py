a87622c09becd8e78e7e3110f312480cc9e5d5f4
nlapalu <nicolas.lapalu@versailles.inra.fr>
2016-04-11 15:06:01 +0200

    update new blast parser, new test
    Signed-off-by: nlapalu <nicolas.lapalu@versailles.inra.fr>

--- setup.py
+++ setup.py
@@ -6,25 +6,32 @@ from unittest import TextTestRunner, TestLoader, TestSuite
 
 from tests.test_AlignDB import TestAlignDB
 from tests.test_AlignmentChainer import TestAlignmentChainer
-from tests.test_BlastXMLParser import TestBlastXMLParser
+from tests.test_BlastTabParser import TestBlastTabParser
+
+try:
+    from tests.test_BlastXMLParser import TestBlastXMLParser
+    Biopython_available = True
+except ImportError:
+    Biopython_available = False
 
 class TestSuite(Command):
     user_options = []
 
     def initialize_options(self):
-        pass 
+        pass
 
     def finalize_options(self):
         pass
 
     def run(self):
 
-        lTestCases = [TestAlignDB,TestAlignmentChainer, TestBlastXMLParser]
+        lTestCases = [TestAlignDB,TestAlignmentChainer, TestBlastTabParser]
+        if Biopython_available:
+            lTestCases.append(TestBlastXMLParser)
         for case in lTestCases:
             suite = unittest.TestLoader().loadTestsFromTestCase(case)
             t = TextTestRunner(verbosity = 2)
             t.run(suite)
-
 
 setup(name='SDDetector',
       version='0.1',
@@ -36,7 +43,7 @@ setup(name='SDDetector',
       scripts=['bin/segmental_duplication_detector.py'],
       packages=['SDDetector.Db','SDDetector.Utils','SDDetector.Parser',
                 'SDDetector.Parser.Blast', 'SDDetector.Entities','SDDetector', 'tests'],
-      data_files=[('test-data', ['test-data/blast.xml']),
+      data_files=[('test-data', ['test-data/blast.xml','test-data/blast.tab']),
                   ('.',['README.md'])],
       cmdclass={
           'test': TestSuite
