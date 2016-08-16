8959adfa1296d733d58ce70cea7e8afd9dd5d818
nlapalu <nicolas.lapalu@versailles.inra.fr>
2016-08-16 17:01:22 +0200

    refactoring

--- bin/segmental_duplication_gene_analyzer.py
+++ bin/segmental_duplication_gene_analyzer.py
@@ -3,17 +3,20 @@
 import argparse
 import logging
 import os
+import sys
 
 from SDDetector.Entities.Region import Region
 from SDDetector.Entities.GeneLink import GeneLink
 
 from SDDetector.Parser.Gff.GffDuplicationParser import GffDuplicationParser
 from SDDetector.Parser.Gff.GffGeneParser import GffGeneParser
+from SDDetector.Parser.Gff.GffTEParser import GffTEParser
 from SDDetector.Parser.Blast.BlastXMLParser import BlastXMLParser
 
 from SDDetector.Db.GeneDB import GeneDB
 
 from SDDetector.Utils.CircosPlot import CircosPlot
+from SDDetector.Utils.FastaFileIndexer import FastaFileIndexer
 
 try:
     from SDDetector.Parser.Blast.BlastXMLParser import BlastXMLParser
@@ -23,18 +26,26 @@ except ImportError:
     exit(1)
 
 
-class GeneAnalyzer(object):
+class Analyzer(object):
 
-    def __init__(self, SDFile='',BlastXMLFile='',GeneFile='', logLevel='ERROR'):
+    def __init__(self, SDFile='', BlastXMLFile='', GeneFile='', outputFile='', logLevel='ERROR'):
         """Constructor"""
 
         self.SDFile = SDFile
         self.BlastXMLFile = BlastXMLFile
         self.GeneFile = GeneFile
+        self.outputFile = outputFile
         self.logLevel = logLevel
         logging.basicConfig(level=self.logLevel)
 
         self.lDuplications = []
+
+
+    def __del__(self):
+        """Destructor"""
+
+        if os.path.exists('gene.db'): 
+            os.remove('gene.db')
 
 
     def parseAttributesFromArgsCLI(self):
@@ -51,10 +62,14 @@ class GeneAnalyzer(object):
         parser.add_argument("SDFile", help="Segmental Duplication gff3 file = output of SDDetector (filtered or not)", type=str)
         parser.add_argument("BlastXMLFile", help="Input Blast XML file", type=str)
         parser.add_argument("GeneFile", help="gene annotation file in gff3 format", type=str)
-        #parser.add_argument("outputFile", help="Output File in gff3 format", type=str)
+        parser.add_argument("outputFile", help="Output File", type=str)
 
         parser.add_argument("-v", "--verbosity", type=int, choices=[1,2,3],
                             help="increase output verbosity 1=error, 2=info, 3=debug")
+        parser.add_argument("-t", "--TEFile", type=str, default=None, help="Transposable \
+                            elements / Repeats file in gff3 format")
+        parser.add_argument("-g", "--GenomeFile", type=str, default=None, help="Genome \
+                            fasta file, required for circos plot")
         parser.add_argument("--circos", action="store_true", help="Write circos \
                             configuration file and associated data files")
 
@@ -76,62 +91,70 @@ class GeneAnalyzer(object):
         self.SDFile = args.SDFile
         self.BlastXMLFile = args.BlastXMLFile
         self.GeneFile = args.GeneFile
+        self.TEFile = args.TEFile
         self.circos = args.circos
+        self.GenomeFile = args.GenomeFile
+        self.outputFile = args.outputFile
 
 
     def getPolymorphismEffect(self):
         """Analyze polymorphism between genes and return list of variants"""
-        
-        for link in self.lGeneLinks:
-            # analyse CDS Share Alignment
-            print 'Gene: ({},{}); sequence: ({},{}); strand: ({},{})'.format(link.gene1.id, link.gene2.id,link.gene1.seqid,link.gene2.seqid,link.gene1.strand,link.gene2.strand)
-            #print 'Algmt: {}'.format(link.getCDSAlignment())
-            #print 'Effect: {}'.format(link.getEffect())
-            lAlignEffect, nbMutations, r1, r2 = link.getEffect()
+       
+        with open(self.outputFile,'w') as f:
+            logging.info('Writing polymorphism effect in {}'.format(self.outputFile))
+            for link in self.lGeneLinks:
+                # analyse CDS Share Alignment
+                f.write('Gene: ({},{}); sequence: ({},{}); strand: ({},{})'.format(link.gene1.id, link.gene2.id,link.gene1.seqid,link.gene2.seqid,link.gene1.strand,link.gene2.strand))
+                #print 'Algmt: {}'.format(link.getCDSAlignment())
+                #print 'Effect: {}'.format(link.getEffect())
+                lAlignEffect, lMutations, r1, r2 = link.getEffect()
+                for strMutation in lMutations:
+                    f.write(strMutation)
 
-            nbBases = len(lAlignEffect[0])
-            size = 60
-            indexSize = 0
-            indexBase = 0
-            algmtGene = ''
+                nbBases = len(lAlignEffect[0])
+                size = 60
+                indexSize = 0
+                indexBase = 0
+                algmtGene = ''
 
-            if link.gene1.strand == 1:
-                algmt1Start, algmt1End = (r1.start, r1.end)
-            else:
-                algmt1Start, algmt1End = (r1.end, r1.start)
-            if link.gene2.strand == 1:
-                algmt2Start, algmt2End = (r2.start, r2.end)
-            else:
-                algmt2Start, algmt2End = (r2.end, r2.start)
+                if link.gene1.strand == 1:
+                    algmt1Start, algmt1End = (r1.start, r1.end)
+                else:
+                    algmt1Start, algmt1End = (r1.end, r1.start)
+                if link.gene2.strand == 1:
+                    algmt2Start, algmt2End = (r2.start, r2.end)
+                else:
+                    algmt2Start, algmt2End = (r2.end, r2.start)
             
 
-            start1 = algmt1Start
-            start2 = algmt2Start
-            end1 = 0
-            end2 = 0
-            while indexBase < nbBases:
-                nbHyphen1 = lAlignEffect[2][indexBase:indexBase+size].count('-')
-                nbHyphen2 = lAlignEffect[4][indexBase:indexBase+size].count('-')
+                start1 = algmt1Start
+                start2 = algmt2Start
+                end1 = 0
+                end2 = 0
+                while indexBase < nbBases:
+                    nbHyphen1 = lAlignEffect[2][indexBase:indexBase+size].count('-')
+                    nbHyphen2 = lAlignEffect[4][indexBase:indexBase+size].count('-')
                 
-                if link.gene1.strand == -1:
-                    end1 = start1-size-nbHyphen1
-                else:
-                    end1 = start1+size-nbHyphen1
-                if link.gene2.strand == -1:
-                    end2 = start2-size-nbHyphen2
-                else:
-                    end2 = start2+size-nbHyphen2
+                    if link.gene1.strand == -1:
+                        end1 = start1-size-nbHyphen1
+                    else:
+                        end1 = start1+size-nbHyphen1
+                    if link.gene2.strand == -1:
+                        end2 = start2-size-nbHyphen2
+                    else:
+                        end2 = start2+size-nbHyphen2
                 
-                scale1 = str(start1) + ' '*(size-len(str(start1))-len(str(end1))) + str(end1)
-                scale2 = str(start2) + ' '*(size-len(str(start2))-len(str(end2))) + str(end2)
+                    scale1 = str(start1) + ' '*(size-len(str(start1))-len(str(end1))) + str(end1)
+                    scale2 = str(start2) + ' '*(size-len(str(start2))-len(str(end2))) + str(end2)
 
 
-                algmtGene += '{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n\n'.format(scale1,lAlignEffect[0][indexBase:indexBase+size],lAlignEffect[1][indexBase:indexBase+size],lAlignEffect[2][indexBase:indexBase+size],lAlignEffect[3][indexBase:indexBase+size],lAlignEffect[4][indexBase:indexBase+size],lAlignEffect[5][indexBase:indexBase+size],lAlignEffect[6][indexBase:indexBase+size],scale2)
-                indexBase += size
-                start1 = end1+1
-                start2 = end2+1
+                    algmtGene += '{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n\n'.format(scale1,lAlignEffect[0][indexBase:indexBase+size],lAlignEffect[1][indexBase:indexBase+size],lAlignEffect[2][indexBase:indexBase+size],lAlignEffect[3][indexBase:indexBase+size],lAlignEffect[4][indexBase:indexBase+size],lAlignEffect[5][indexBase:indexBase+size],lAlignEffect[6][indexBase:indexBase+size],scale2)
+                    indexBase += size
+                    start1 = end1+1
+                    start2 = end2+1
 
-            print algmtGene
+                f.write(algmtGene)
+        f.close()
 
 
 
@@ -150,7 +173,7 @@ class GeneAnalyzer(object):
         logging.info('Parsing Blast xml file')
         iBlastXMLParser = BlastXMLParser(self.BlastXMLFile)
         lAlignmentTuples = iBlastXMLParser.getAlignmentsFromTupleOfRegions(lRegions)
-        print lAlignmentTuples
+        #print lAlignmentTuples
 
         index = 0
         for dup in self.lDuplications:
@@ -165,27 +188,76 @@ class GeneAnalyzer(object):
         logging.info('Parsing Gene gff file')
         iGffGeneParser = GffGeneParser(self.GeneFile)
         self.db = GeneDB(dbfile='gene.db')
-        self.db.insertlGenes(iGffGeneParser.getAllGenes())
+        self.lGenes = iGffGeneParser.getAllGenes()
+        self.db.insertlGenes(self.lGenes)
         
         self.lGeneLinks = [] 
         for dup in self.lDuplications:
             (lGeneSeq1,lGeneSeq2) = self._extractGeneInDuplication(dup)
             self.lGeneLinks.extend(self._buildGeneLinks(lGeneSeq1,lGeneSeq2,dup))
 
+        self.getPolymorphismEffect()
+
         if self.circos:
+
+            if self.GenomeFile:
+                logging.info('Indexing Genome fasta file')
+                iFastaGenomeParser = FastaFileIndexer(self.GenomeFile)
+                iFastaGenomeParser.read()
+                ##TODO: modify parser : implement an iterator on sequence/ obj seq with size ?
+                lSeqNames = iFastaGenomeParser.lSeq
+                self.lSeqs = [ (seq, len(iFastaGenomeParser.dSeq[seq])) for seq in lSeqNames ]
+            else:
+                logging.error('Missing Genome File - required for Circos plot')
+                sys.exit(1)
+        
+            if self.TEFile:
+                logging.debug('Parsing TE gff file')
+                parser = GffTEParser(self.TEFile)
+                self.lTEs = parser.getAllTEs()
+
             logging.info('Generating circos files')
             self.writeCircosPlotFiles()
 
 
-        self.getPolymorphismEffect()
 
     def writeCircosPlotFiles(self):
         """write circos files"""
 
         cPlot = CircosPlot()
 
+        if self.lSeqs:
+            SeqDataFile = 'genome.txt'
+            logging.info('Writing circos sequence data file in {}'.format(SeqDataFile))
+            cPlot.writeSeqDataFile(self.lSeqs, SeqDataFile)
+
+        if self.lDuplications:
+            SDDataFile = 'segdup.txt' 
+            logging.info('Writing circos SD data file in {}'.format(SDDataFile))
+            cPlot.writeSegDupDataFile(self.lDuplications, SDDataFile)
+            SimilarityDataFile = 'similarity.txt'
+            logging.info('Writing circos similarity data file in {}'.format(SimilarityDataFile))
+            cPlot.writeSimilarityDataFile(self.lDuplications, SimilarityDataFile)
+
+        if self.lGenes:
+            GeneDataFile = 'gene.txt'
+            logging.info('Writing circos gene data file in {}'.format(GeneDataFile))
+            cPlot.writeGeneDataFile(self.lGenes, GeneDataFile)
+
         if self.lGeneLinks:
-            cPlot.writeGeneLinkDataFile(self.lGeneLinks) 
+            GeneLinkDataFile = 'gene-link.txt'
+            logging.info('Writing circos gene-link data file in {}'.format(GeneLinkDataFile))
+            cPlot.writeGeneLinkDataFile(self.lGeneLinks, GeneLinkDataFile)
+       
+        if self.lTEs:
+            TEDataFile = 'TE.txt'
+            logging.info('Writing circos TE/Repeat data file in {}'.format(TEDataFile))
+            cPlot.writeTEDataFile(self.lTEs, TEDataFile)
+
+        CircosConfFile = 'circos.conf'
+        logging.info('Writing circos configuration file in {}'.format(CircosConfFile))
+        cPlot.writeCircosConf()
+
 
 
     def _buildGeneLinks(self,lGeneSeq1,lGeneSeq2,dup):
@@ -193,7 +265,7 @@ class GeneAnalyzer(object):
 
         lLinks = []
         for gene1 in lGeneSeq1:
-            print gene1.id
+            #print gene1.id
             (seq2ID,val1) = dup.dSeqToSeq[gene1.seqid][gene1.start]
             (seq2ID,val2) = dup.dSeqToSeq[gene1.seqid][gene1.end]
             seq2Start = min(val1,val2)
@@ -204,7 +276,7 @@ class GeneAnalyzer(object):
                 if (gene2.start < seq2Start and gene2.end < seq2Start) or (gene2.start > seq2End and gene2.end > seq2End):
                     next
                 else:
-                   print 'gene1 -  gene2 : {} {}'.format(gene1.id,gene2.id)
+                   #print 'gene1 -  gene2 : {} {}'.format(gene1.id,gene2.id)
                    lLinks.append(GeneLink(dup=dup,gene1=gene1,gene2=gene2)) 
         return lLinks        
         # todo set : + logging.debug
@@ -218,36 +290,8 @@ class GeneAnalyzer(object):
 
         return (lGeneSeq1,lGeneSeq2)
 
-
-
-
-
-#    def _buildGeneLinks(self,lGeneSeq1,lGeneSeq2,dup):
-#        """build"""
-#
-#        lLinks = []
-#        for gene1 in lGeneSeq1:
-#            seq2Start = self._getAlgmtCorrespondingBase()
-#            seq2End = 
-#            for gene2 in lGeneSeq2:
-#                if (gene2.start < seq2Start and gene2.end < seq2Start) or (gene2.start > seq2End and gene2.end > seq2End):
-#                    next
-#                else:
-#                   lLinks.append(GeneLink(dup=dup,gene1=gene1,gene2=gene2)) 
-                
-        # todo set : + logging.debug
-      
-
-#    def _extractGeneInDuplication(self,dup):
-#        """extract """
-
-#        lGeneSeq1 = self.db.getlGenesFromCoordinates(dup.seq1,dup.start1,dup.end1)
-#        lGeneSeq2 = self.db.getlGenesFromCoordinates(dup.seq2,dup.start2,dup.end2)
-
-#        return (lGeneSeq1,lGeneSeq2)
-
 if __name__ == "__main__":
 
-    analyzer = GeneAnalyzer()
+    analyzer = Analyzer()
     analyzer.parseAttributesFromArgsCLI()
     analyzer.runAnalyze()
--- tests/test_CircosPlot.py
+++ tests/test_CircosPlot.py
@@ -3,27 +3,34 @@
 import unittest
 import filecmp
 
+
+from SDDetector.Entities.Duplication import Duplication
+from SDDetector.Entities.Region import Region
+from SDDetector.Entities.Gene import Gene
+from SDDetector.Entities.GeneLink import GeneLink
+from SDDetector.Entities.Feature import Feature
 from SDDetector.Utils.CircosPlot import CircosPlot
 
 class TestCircosPlot(unittest.TestCase):
 
     def setUp(self):
 #        GenomeFile = ('test-data/test2.fasta')
-        GenomeFile = ('test-data/test.fasta')
+#        GenomeFile = ('test-data/test.fasta')
 #        SDFile = ('test-data/sdd4.gff3')
-        SDFile = ('test-data/sdd3.gff3')
+#        SDFile = ('test-data/sdd3.gff3')
 #        GeneFile = ('test-data/gene2.gff3')
-        GeneFile = ('test-data/gene.gff3')
+#        GeneFile = ('test-data/gene.gff3')
 #        TEFile = ('test-data/TE2.gff3')
-        TEFile = ('test-data/TE.gff3')
+#        TEFile = ('test-data/TE.gff3')
 #        BlastXMLFile = ('test-data/blast4.xml')
-        BlastXMLFile = ('test-data/blast2.xml')
-        self.plot = CircosPlot(GenomeFile=GenomeFile,SDFile=SDFile,GeneFile=GeneFile,TEFile=TEFile,BlastXMLFile=BlastXMLFile)
+#        BlastXMLFile = ('test-data/blast2.xml')
+#        self.plot = CircosPlot(GenomeFile=GenomeFile,SDFile=SDFile,GeneFile=GeneFile,TEFile=TEFile,BlastXMLFile=BlastXMLFile)
+         self.plot = CircosPlot()
 
     def tearDown(self):
         pass
 
-#    def test_writeCircosConf(self):
+    def test_writeCircosConf(self):
         """Test writeCircosConf"""
        
 #        self.plot.writeSeqDataFile() 
@@ -32,43 +39,81 @@ class TestCircosPlot(unittest.TestCase):
 #        self.plot.writeTEDataFile() 
 #        self.plot.writeCircosConf()
 
+        lSeqs = [('seq1',20000),('seq2',30000)] 
+        GenomeDataFile = self.plot.writeSeqDataFile(lSeqs, 'genome.txt')
+        lRegions = [(Region('seq1',100,220,1),Region('seq2',100,220,1)),
+                    (Region('seq1',1200,1300,-1),Region('seq2',1300,1400,1))] 
+        lAlgmts = [('ATGCATGCATGCATGCATGCATGCATGCATGCATGCAGGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATATGTGTAGTGAGTCGTCCC',
+                    'ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGATGTACGATATAGCCCAC'),
+                   ('ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAA',
+                    'ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAA')]
+        lDuplications = [Duplication('seq1',1,5000,'seq2',1,6000,lRegions,lAlgmts)]
+        SDDataFile = self.plot.writeSegDupDataFile(lDuplications,'segdup.txt') 
+        self.plot.writeCircosConf()
+        self.assertTrue(filecmp.cmp('circos.conf','test-data/circos.conf'))
+        lGenes = [Gene('GENE1','seq1',12,600,1),Gene('GENE2','seq2',100,1000,-1)]
+        GeneDataFile = self.plot.writeGeneDataFile(lGenes,'gene.txt') 
+        lTEs = [Feature('TE1','seq1',1000,2000,1,'TE'),Feature('TE2','seq2',4000,4500,-1,'TE')]
+        TEDataFile = self.plot.writeTEDataFile(lTEs,'TE.txt')
+        self.plot.writeCircosConf()
+        self.assertTrue(filecmp.cmp('circos.conf','test-data/circos2.conf'))
+        SimilarityDataFile = self.plot.writeSimilarityDataFile(lDuplications,'similarity.txt')
+        self.plot.writeCircosConf()
+        self.assertTrue(filecmp.cmp('circos.conf','test-data/circos3.conf'))
+
+
     def test_writeSeqDataFile(self):
         """Test writeSeqDataFile"""
 
-        GenomeDataFile = self.plot.writeSeqDataFile()
+        lSeqs = [('seq1',4),('seq2',8)] 
+        GenomeDataFile = self.plot.writeSeqDataFile(lSeqs, 'genome.txt')
         self.assertTrue(filecmp.cmp(GenomeDataFile,'test-data/genome.txt'))
 
-#    def test_writeSegDupDataFile(self):
-#        """Test writeSegDupDataFile"""
+    def test_writeSegDupDataFile(self):
+        """Test writeSegDupDataFile"""
 
-#        SDDataFile = self.plot.writeSegDupDataFile() 
-#        self.assertTrue(filecmp.cmp(SDDataFile,'test-data/segdup.txt'))
+        lDuplications = [Duplication('seq1',5,5000,'seq2',10,5000)]
+        SDDataFile = self.plot.writeSegDupDataFile(lDuplications,'segdup.txt') 
+        self.assertTrue(filecmp.cmp(SDDataFile,'test-data/segdup.txt'))
 
 
     def test_writeGeneDataFile(self):
         """Test writeGeneDataFile"""
 
-        GeneDataFile = self.plot.writeGeneDataFile() 
+        lGenes = [Gene('GENE1','seq1',12,600,1),Gene('GENE2','seq2',100,1000,-1)]
+        GeneDataFile = self.plot.writeGeneDataFile(lGenes,'gene.txt') 
         self.assertTrue(filecmp.cmp(GeneDataFile,'test-data/gene.txt'))
 
     def test_writeGeneLinkDataFile(self):
         """Test writeGeneLinkDataFile"""
 
-        GeneLinkDataFile = self.plot.writeGeneLinkDataFile()
+        iDup = Duplication('seq1',5,5000,'seq2',10,5000)
+        iGene1 = Gene('GENE1','seq1',10,100,1)
+        iGene2 = Gene('GENE2','seq2',100,190,-1)
+        lGeneLinks = [GeneLink(dup=iDup,gene1=iGene1,gene2=iGene2)]
+        GeneLinkDataFile = self.plot.writeGeneLinkDataFile(lGeneLinks,'gene-link.txt')
         self.assertTrue(filecmp.cmp(GeneLinkDataFile,'test-data/gene-link.txt'))
 
     def test_writeTEDataFile(self):
         """Test writeTEDataFile"""
 
-        TEDataFile = self.plot.writeTEDataFile()
+        lTEs = [Feature('TE1','seq1',1000,2000,1,'TE'),Feature('TE2','seq2',4000,4500,-1,'TE')]
+        TEDataFile = self.plot.writeTEDataFile(lTEs,'TE.txt')
         self.assertTrue(filecmp.cmp(TEDataFile, 'test-data/TE.txt'))
 
 
-#    def test_writeSimilarityDataFile(self):
-#        """Test writeSimilarityDataFile"""
+    def test_writeSimilarityDataFile(self):
+        """Test writeSimilarityDataFile"""
 
-#        SimilarityDataFile = self.plot.writeSimilarityDataFile()
-#        self.assertTrue(filecmp.cmp(SimilarityDataFile,'test-data/similarity.txt'))
+        lRegions = [(Region('seq1',100,220,1),Region('seq2',100,220,1)),
+                    (Region('seq1',1200,1300,-1),Region('seq2',1300,1400,1))] 
+        lAlgmts = [('ATGCATGCATGCATGCATGCATGCATGCATGCATGCAGGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATATGTGTAGTGAGTCGTCCC',
+                    'ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGATGTACGATATAGCCCAC'),
+                   ('ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAA',
+                    'ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAATGCATGCATGCATGCATGCATGCATGCATGCATGCATGAA')]
+        lDuplications = [Duplication('seq1',1,5000,'seq2',1,6000,lRegions,lAlgmts)]
+        SimilarityDataFile = self.plot.writeSimilarityDataFile(lDuplications,'similarity.txt')
+        self.assertTrue(filecmp.cmp(SimilarityDataFile,'test-data/similarity.txt'))
 
 
 if __name__ == "__main__":
