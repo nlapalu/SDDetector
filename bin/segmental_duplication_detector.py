234eb6eb895524609d5a44705f977cbc20850fb6
nlapalu <nicolas.lapalu@versailles.inra.fr>
2017-06-15 15:52:16 +0200

    update and clean

--- bin/segmental_duplication_detector.py
+++ bin/segmental_duplication_detector.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+"""
+   segmental_duplication_detector
+"""
+
 import argparse
 import logging
 import os
@@ -13,17 +17,31 @@ from SDDetector.Parser.Blast.BlastXMLParserExpat import BlastXMLParserExpat
 
 class Detector(object):
 
-    def __init__(self, db='', inputFile='', inputFormat='', outputFile='',logLevel='ERROR'):
+
+    def __init__(self, db='', inputFile='', inputFormat='', outputFile='',
+                 minIdentity=0.9, maxGap=3000, chainLength=5000, matchLength=0,
+                 matchOverlap=0, keepOverDup=False, keepInternSimDup=False,
+                 exportDBAllSteps=False, exportBed=False, logLevel='ERROR'):
         """Constructor"""
 
         self.dbFile = db
         self.inputFile = inputFile
         self.inputFormat = inputFormat
         self.outputFile = outputFile
+        self.minIdentity = minIdentity
+        self.maxGap = maxGap
+        self.chainLength = chainLength
+        self.matchLength = matchLength
+        self.matchOverlap = matchOverlap
+        self.keepOverDup = keepOverDup
+        self.keepInternSimDup = keepInternSimDup
+        self.exportDBAllSteps = exportDBAllSteps
+        self.exportBed = exportBed
         self.db = None
         self.parser = None
         self.logLevel = logLevel
         logging.basicConfig(level=self.logLevel)
+
 
     def __del__(self):
         """Destructor"""
@@ -31,89 +49,6 @@ class Detector(object):
         if (self.db and self.dbFile != ':memory:'):
             self.db.deleteDB()
 
-    def parseAttributesFromArgsCLI(self):
-        """Parse arguments from command line"""
-
-        program = 'SDDetector'
-        version = __version__
-        description = "SDDetector: detects segmental duplications in genome"
-
-        parser = argparse.ArgumentParser(prog=program)
-        parser = argparse.ArgumentParser(description=description)
-        parser.add_argument('--version', action='version', version='{} {}'.format(program,version))
-
-        parser.add_argument("inputFile", help="Input Blast file", type=str)
-        parser.add_argument("inputFormat", help="Input Blast format [xml|tab]", type=str)
-        parser.add_argument("outputFile", help="Output File in gff3 format", type=str)
-        parser.add_argument("db", help="file to store SQLite db, ex: mydb.db, or ':memory:' \
-                                        to keep db in RAM", type=str)
-
-        parser.add_argument("-v", "--verbosity", type=int, choices=[1,2,3],
-                            help="increase output verbosity 1=error, 2=info, 3=debug")
-        parser.add_argument("-i", "--minIdent", type=float, default=0.9,
-                            help="minimum alignment percentage identity, [default=0.9], range [0-1]")
-        parser.add_argument("-g", "--maxGap", type=int, default=3000,
-                            help="maximum gap size allowed to chain two fragments, in bp \
-                            [default=3000]")
-        parser.add_argument("-l", "--chainLength", type=int, default=5000,
-                            help="minimum chain length (without gaps) to keep, in bp \
-                            [default=5000]")
-        parser.add_argument("-t", "--matchLength", type=int, default=0,
-                            help="minimum match length to consider [default=0]")
-        parser.add_argument("-p","--matchOverlap", type=int, default=0,
-                            help="maximum overlap to consider between 2 alignments \
-                            before removing suboptimal alignment [default=0]")
-        parser.add_argument("-r", "--keepOverDup", action="store_true", default=False,
-                            help="keep overlapping duplications, [default=False] only the longest is kept")
-        parser.add_argument("-s", "--keepInternSimDup", action="store_true", default=False,
-                            help="keep duplication with internal similarity, [default=False]")
-        parser.add_argument("-a", "--exportall", action="store_true",
-                            help="export the content of the db at each step: all, removing \
-                            self-matches, identity threshold, suboptimal matches")
-        parser.add_argument("-b", "--bed", action="store_true",
-                            help="export also results in bed format")
-
-        args = parser.parse_args()
-        self._setAttributesFromArgsCLI(args)
-
-    def _setAttributesFromArgsCLI(self, args):
-        """Set attributes from argparse"""
-
-        if args.verbosity == 1:
-            self.logLevel = 'ERROR'
-        if args.verbosity == 2:
-            self.logLevel = 'INFO'
-        if args.verbosity == 3:
-            self.logLevel = 'DEBUG'
-        logging.getLogger().setLevel(self.logLevel)
-
-        self.inputFormat = args.inputFormat.lower()
-
-        if self.inputFormat not in ['xml','tab']:
-            raise Exception('untractable blast format')
-            exit(1)
-
-        self.inputFile= args.inputFile
-        self.outputFile = args.outputFile
-
-        self.dbFile = args.db
-        if self.dbFile == ':memory:':
-            logging.debug("SQLite db stored in memory")
-        else:
-            logging.debug("SQLite db stored in {}".format(self.dbFile))
-
-        if (args.minIdent < 0 or args.minIdent > 1):
-            raise Exception('minimum identity not in range [0-1], example: 0.95')
-        else:
-            self.minIdentity = args.minIdent
-        self.maxGap = args.maxGap
-        self.chainLength = args.chainLength
-        self.matchLength = args.matchLength
-        self.matchOverlap = args.matchOverlap
-        self.keepOverDup = args.keepOverDup
-        self.keepInternSimDup = args.keepInternSimDup
-        self.exportDBAllSteps = args.exportall
-        self.exportBed = args.bed
 
     def runSDDetection(self):
         """run segmental duplication detection"""
@@ -134,7 +69,7 @@ class Detector(object):
         if self.exportDBAllSteps:
             logging.info('Exporting matches after removing self-matches in gff3 format, file: {}.selfmatch'.format(self.outputFile))
             self.exportMatches('{}.selfmatch'.format(self.outputFile))
-        logging.info('Removing Alignment below the identity threshold: {}'.format(self.minIdentity))
+        logging.info('Removing alignments below the identity threshold: {}'.format(self.minIdentity))
         self.detectAndRemoveAlignmentBelowIdentityThreshold()
         if self.exportDBAllSteps:
             logging.info('Exporting matches after removing mimimal identity in gff3 format, file: {}.minidentity'.format(self.outputFile))
@@ -144,10 +79,10 @@ class Detector(object):
         if self.exportDBAllSteps:
             logging.info('Exporting matches after removing suboptimal alignments in gff3 format, file: {}.suboptimal'.format(self.outputFile))
             self.exportMatches('{}.suboptimal'.format(self.outputFile))
-        logging.info('Chaining alignments with parameters: maximum Gap between fargments = {} bp, minimum chain length = {} bp'.format(self.maxGap, self.chainLength))
+        logging.info('Chaining alignments with parameters: maximum Gap between fragments = {} bp, minimum chain length = {} bp'.format(self.maxGap, self.chainLength))
         self.chainAlignments(maxGap=self.maxGap, chainLength=self.chainLength)
         if not self.keepOverDup:
-            logging.info('Removing overlapping duplications: only the longest one is keep')
+            logging.info('Removing overlapping duplications: only the longest one is kept')
             self.removeOverlappingDuplications()
         if not self.keepInternSimDup:
             logging.info('Removing intra-sequence duplications with internal similarity')
@@ -162,11 +97,6 @@ class Detector(object):
     def parseAlignments(self):
         """Parse Alignments"""
 
-#        if self.inputFormat == 'xml' and Biopython_available == True:
-#            #self.parser = BlastXMLParser(self.inputFile)
-#        elif self.inputFormat == 'xml' and Biopython_available == False:
-#            raise Exception('BioPython is not installed, xml parsing not available. \
-#                             Please choose tab format, or install BioPython')
         if self.inputFormat == 'xml':
             self.parser = BlastXMLParserExpat(self.inputFile)
         elif self.inputFormat == 'tab':
@@ -207,6 +137,7 @@ class Detector(object):
         self.db.deletelAlignments(lAlgmts)
         self.db.commit()
 
+
     def detectAndRemoveSuboptimalAlignments(self):
         """Detect and remove suboptimal alignment"""
 
@@ -215,15 +146,9 @@ class Detector(object):
         self.db.deletelAlignments(lAlgmts)
         self.db.commit()
 
+
     def chainAlignments(self, maxGap=3000, chainLength=5000):
-        """Chain Alignments
-
-           - sort by sbjct, query ann coordiantes
-           - chaine if distance < 3000 nt
-           - remove if chained alignment < 5000 bp
-           - ??? distance on query ???
-
-        """
+        """Chain Alignments"""
 
         lSbjcts = self.db.selectAllSbjcts()
         lQueries = self.db.selectAllQueries()
@@ -242,17 +167,20 @@ class Detector(object):
         chainer2 = AlignmentChainer(self.db, maxGap=maxGap)
         self.lSortedChains = chainer2.sortListOfChains(lSelectedChains)
 
+
     def removeOverlappingDuplications(self):
         """Remove Overlapping Duplications"""
 
         chainer = AlignmentChainer(self.db)
         self.lSortedChains = chainer.removeOverlappingChains(self.lSortedChains)
 
+
     def removeDuplicationWithInternalSimilarity(self):
         """Remove Duplication with internal similarity"""
 
         chainer = AlignmentChainer(self.db)
         self.lSortedChains = chainer.removeChainsWithInternalSimilarity(self.lSortedChains)
+
 
     def exportChains(self, outputFile, format='gff3'):
         """Export Chains in gff3|bed format"""
@@ -280,6 +208,73 @@ class Detector(object):
 
 if __name__ == "__main__":
 
-    detector = Detector()
-    detector.parseAttributesFromArgsCLI()
+    program = 'SDDetector'
+    version = __version__
+    description = "SDDetector: detects segmental duplications in genome"
+
+    parser = argparse.ArgumentParser(prog=program)
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--version', action='version', version='{} {}'.format(program,version))
+    parser.add_argument("inputFile", help="Input Blast file", type=str)
+    parser.add_argument("inputFormat", help="Input Blast format [xml|tab]", type=str)
+    parser.add_argument("outputFile", help="Output File in gff3 format", type=str)
+    parser.add_argument("db", help="file to store SQLite db, ex: mydb.db, or ':memory:' \
+                        to keep db in RAM", type=str)
+    parser.add_argument("-v", "--verbosity", type=int, choices=[1,2,3],
+                        help="increase output verbosity 1=error, 2=info, 3=debug")
+    parser.add_argument("-i", "--minIdent", type=float, default=0.9,
+                        help="minimum alignment percentage identity, [default=0.9], range [0-1]")
+    parser.add_argument("-g", "--maxGap", type=int, default=3000,
+                        help="maximum gap size allowed to chain two fragments, in bp \
+                        [default=3000]")
+    parser.add_argument("-l", "--chainLength", type=int, default=5000,
+                        help="minimum chain length (without gaps) to keep, in bp \
+                        [default=5000]")
+    parser.add_argument("-t", "--matchLength", type=int, default=0,
+                        help="minimum match length to consider [default=0]")
+    parser.add_argument("-p","--matchOverlap", type=int, default=0,
+                        help="maximum overlap to consider between 2 alignments \
+                        before removing suboptimal alignment [default=0]")
+    parser.add_argument("-r", "--keepOverDup", action="store_true", default=False,
+                        help="keep overlapping duplications, [default=False] only the longest is kept")
+    parser.add_argument("-s", "--keepInternSimDup", action="store_true", default=False,
+                        help="keep duplication with internal similarity, Usefull for \
+                        closed tandem duplication connected in one duplication [default=False]")
+    parser.add_argument("-a", "--exportall", action="store_true", default=False,
+                        help="export the content of the db at each step: all, removing \
+                        self-matches, identity threshold, suboptimal matches")
+    parser.add_argument("-b", "--bed", action="store_true", default=False,
+                        help="export also results in bed format")
+    args = parser.parse_args()
+
+
+    if args.verbosity == 1:
+        logLevel = 'ERROR'
+    if args.verbosity == 2:
+        logLevel = 'INFO'
+    if args.verbosity == 3:
+        logLevel = 'DEBUG'
+    logging.getLogger().setLevel(logLevel)
+
+    inputFormat = args.inputFormat.lower()
+
+    if inputFormat not in ['xml','tab']:
+        raise Exception('untractable blast format')
+        sys.exit(1)
+
+    if args.db == ':memory:':
+        logging.info("SQLite db stored in memory")
+    else:
+        logging.info("SQLite db stored in {}".format(args.db))
+
+    if (args.minIdent < 0 or args.minIdent > 1):
+        raise Exception('minimum identity not in range [0-1], example: 0.95')
+
+    detector = Detector(args.db, args.inputFile, inputFormat, args.outputFile,
+                        minIdentity=args.minIdent, maxGap=args.maxGap,
+                        chainLength=args.chainLength, matchLength=args.matchLength,
+                        matchOverlap=args.matchOverlap, keepOverDup=args.keepOverDup,
+                        keepInternSimDup=args.keepInternSimDup,
+                        exportDBAllSteps=args.exportall, exportBed=args.bed,
+                        logLevel=logLevel)
     detector.runSDDetection()
